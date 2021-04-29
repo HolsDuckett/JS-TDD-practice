4c4d992e2f2e6de6559326db08e0187531155327
Holly Duckett <hollyduckett@hollys-mbp.home>
2021-04-29 11:04:50 +0100

    2nd test pass - 2 character string returns both characters

--- middleLetter.test.js
+++ middleLetter.test.js
@@ -5,4 +5,8 @@ describe('Find the middle letter', () => {
     test('returns the string when only 1 character string is given', () => {
         expect(middleLetter("A")).toBe("A");
     });
+
+    test('returns full string when only a 2 character string is passed', () => {
+        expect(middleLetter("BB")).toBe("BB");
+    });
 })
